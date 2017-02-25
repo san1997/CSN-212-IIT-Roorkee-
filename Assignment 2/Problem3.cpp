0f6abe029caf76779e6216af49ce392889f5d835
Sandeep Pal <sanpal1997@gmail.com>
2017-02-25 19:15:51 +0530

    Create Problem3.cpp

---
+++ Assignment 2/Problem3.cpp
@@ -0,0 +1,44 @@
+//circular LIS
+#include <bits/stdc++.h>
+using namespace std;
+int n,a[30002];
+int len(int p)
+{
+	int dp[30002];
+	for(int i=p;i<p+n;i++)
+	{
+		int maxm = 0;
+		for(int j=0;j<i;j++)
+		{
+			if(a[i] > a[j])
+			{
+				if(maxm < dp[j])maxm = dp[j];
+			}
+		}
+		dp[i] = maxm+1;
+	}
+	int maxm = 0;
+	for(int i=p;i<p+n;i++)
+		if(maxm < dp[i])maxm = dp[i];
+	return maxm;
+}
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		cin >> n;
+		for(int i= 0;i<n;i++)
+			{cin >> a[i];a[i+n] = a[i];}
+		int ans = 0;
+		for(int i=0;i<n;i++)
+			{
+				int v = len(i);
+				if(v > ans)ans = v;
+			}
+		cout << ans << endl;	
+		
+	}
+	return 0;
+}
